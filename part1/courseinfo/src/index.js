47f3b62af578b25059dfb03ebc271210bfe18017
Awel Eshetu <awel@arctechno.com>
2019-09-30 10:06:02 +0300

    part1 in progress

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,9 +10,9 @@ const Header=(props)=>{
 }
 const Part=(props)=>{
    return (
-      props.parts.map(part=>
+      props.parts.map((part)=>
         <>
-       <p>{part.name} {part.exercises}</p>
+       <p key={part.name}>{part.name} {part.exercises}</p>
        </>                 
     )
        
@@ -34,26 +34,28 @@ const Total=(props)=>{
     )
 }
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/> 
-      <Content parts={parts}/>
-      <Total total={parts.map(part=>part.exercises)}/>
+      <Header course={course.name}/> 
+      <Content parts={course.parts}/>
+      <Total total={course.parts.map(part=>part.exercises)}/>
     </div>
   )
 }
