f3292ac8a8ab37c2c817f6abb46e0955eb568c03
Awel Eshetu <awel@arctechno.com>
2019-10-05 12:01:39 +0300

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,7 +9,11 @@ const App = () => {
         [bad, setBad] = useState(0),
         handleGood=()=>setGood(good+1),
         handleNeutral=()=>setNeutral(neutral+1),
-        handleBad=()=>setBad(bad+1);
+        handleBad=()=>setBad(bad+1),
+        all=bad+good+neutral,
+        average=(((bad*-1)+good)/all),
+        positive=(good/all)*100;
+    
  const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
     {text}
@@ -17,8 +21,8 @@ const App = () => {
   );
  const containerStyle={
      float:'left',
-     width:'4%',
-     textAlign:'center'
+     overflow:'hidden',
+    textAlign:'centre'
  }
       
   return (
@@ -26,15 +30,15 @@ const App = () => {
       <h1 style={{textAlign:'left'}}>Give Feedback</h1>
       <div style={containerStyle}>
       <Button onClick={handleGood} text='Good' /> 
-      <p>{good}</p>
-      </div>
-      <div style={containerStyle}>
       <Button onClick={handleNeutral} text='Neutral' />
-      <p>{neutral}</p>
-      </div>
-      <div style={containerStyle}>
       <Button onClick={handleBad} text='Bad' />
-      <p>{bad}</p>
+      <h2 >Statistics</h2>
+      <p>good {good}</p>      
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>  
+      <p>all {all}</p>
+      <p>average {average||0}</p>
+      <p>positive {positive||0} %</p>
       </div>
       
     </div>
