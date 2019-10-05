ee48dcd9379e67064415cced3ddd767b8d728ee2
Awel Eshetu <awel@arctechno.com>
2019-10-05 12:47:06 +0300

    unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,31 +6,12 @@ const Button = ({ onClick, text }) => (
     {text}
   </button>
   );
-const Statistics=({bad,good,neutral})=>{
-    const all=bad+good+neutral,
-        average=((good-bad)/all),
-        positive=(good/all)*100;
-    if(all>0){
+const Statistics=({value,text})=>{
      return(
-        <>
-      <h2 >Statistics</h2>
-      <p>good {good}</p>      
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>  
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+        <>     
+       <p>{text} {value}</p>
         </>
     )
-        
-    }else{
-        return(
-            <>
-            <h2 >Statistics</h2>
-            <p>No feedback given</p>
-            </>
-        )  
-}
 }
 const App = () => {
   // save clicks of each button to own state
@@ -39,7 +20,10 @@ const App = () => {
         [bad, setBad] = useState(0),
         handleGood=()=>setGood(good+1),
         handleNeutral=()=>setNeutral(neutral+1),
-        handleBad=()=>setBad(bad+1);
+        handleBad=()=>setBad(bad+1),
+        all=bad+good+neutral,
+        average=((good-bad)/all),
+        positive=(good/all)*100;
         
     
  
@@ -56,9 +40,24 @@ const App = () => {
       <Button onClick={handleGood} text='Good' /> 
       <Button onClick={handleNeutral} text='Neutral' />
       <Button onClick={handleBad} text='Bad' />
-      <Statistics bad={bad} good={good} neutral={neutral}/>
-      </div>
-      
+       {
+       all>0 ? 
+      <>
+      <h2>Statistics</h2>
+       <Statistics text='good' value={good}/>
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={all} />
+      <Statistics text='average' value={average} />
+      <Statistics text='positive' value={`${positive} %`} />
+      </>
+       :
+       <>
+     <h2>Statistics</h2>
+     <p>No feedback given</p>
+      </>
+      }
+      </div>   
     </div>
   )
 }
