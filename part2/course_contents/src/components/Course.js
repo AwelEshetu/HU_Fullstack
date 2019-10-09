ba7dfbaef4a57fc5199f63857a3b76283830b074
Awel Eshetu <awel@arctechno.com>
2019-10-09 13:19:33 +0300

    course_content step9

--- part2/course_contents/src/components/Course.js
+++ part2/course_contents/src/components/Course.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const Course = ({ course,total }) => {
-    console.log(total)
+const Course = ({ courses,total }) => {
   return (
     <>
+      {
+    courses.map(course=>
+    <div key={course.id}>
     <h1>{course.name}</h1>
-    {course.parts.map((part)=>
-       <p key={part.id}>{part.name} {part.exercises}</p>
-       )}
-    <h4>total of {total} exercises</h4>
+    {course.parts.map(part=> <p key={part.id}>{part.name} {part.exercises}</p>)}
+    <h4>total of {course.parts.map(part=>part.exercises).reduce((prev,next)=>prev+next)} exercises</h4>
+    </div>
+     )
+     }
+    
     </>
   )
 }
