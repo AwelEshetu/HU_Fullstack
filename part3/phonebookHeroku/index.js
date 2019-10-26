6fca951384260b9f617fe866adb95881edc0d1ca
Awel Eshetu <awel@arctechno.com>
2019-10-26 15:57:38 +0300

    refactored to support update & delete to db

--- part3/phonebookHeroku/index.js
+++ part3/phonebookHeroku/index.js
@@ -6,7 +6,9 @@ const express = require('express'),
       Person = require('./models/person')
       app = express(),
       PORT = process.env.PORT;
+app.use(express.static('build'))
 app.use(bodyParser.json());
+
 app.use(cors());
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
@@ -14,45 +16,66 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :response-time ms :body '));
 
 
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+// handler of requests with unknown endpoint
+/*const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(unknownEndpoint)*/
 
- app.use(express.static("build"));
+//error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+//send index page
   app.get("/", (req, res) => {
     res.sendFile(path.join((__dirname,"build/index.html")));
   });
 
-
+//provide some info
 app.get('/info',(req,res)=>{
     let d=new Date();
-     res.send(`
+    Person.find({}).then(persons => {
+    res.send(`
      <p>Phonebook has info for ${persons.length}</p>
      <p>${d.toString()}</p>
-`)
+    `)
+  }).catch(error => next(error))
+     
 });
+//get all phonebook persons
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons.map(person => person.toJSON()))
-  })
+  }).catch(error => next(error))
 })
-
+//get specific person
 app.get('/api/persons/:id', (request, response) => {
  Person.findById(request.params.id).then(person => {
-    response.json(person.toJSON())
+    if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => next(error))
   })
-})
 
 
+//save person to phonebook
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
+ /* const body = request.body;
   console.log(JSON.stringify(body))
-  if (!body.name || !body.number) {
+  if (body.name===undefined || body.number) {
     return response.status(400).json({ 
       error: 'The name or number is missing' 
     })
@@ -62,18 +85,34 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number,
     date: new Date()
-  });
-
+  });*/
+const person=new Person({...request.body,date:new Date()})
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON())
-  })
+  }).catch(error=>next(error))
 })
+//update specific person 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+//delete specific person
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
